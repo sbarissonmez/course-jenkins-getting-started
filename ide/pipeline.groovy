7ef2c7e64b907cc56d6ac1e4766b5dc015779ca0
Baris Sonmez <s.barissonmez@gmail.com>
2024-03-03 15:12:34 +0300

    copy pipeline out of jenkins web ui into a pipeline.groovy file to edit in vscode

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/sbarissonmez/course-jenkins-getting-started.git'
+                git branch: 'main', url: 'https://github.com/sbarissonmez/jgsu-spring-petclinic.git'
             }
         }    
         stage('Build') {
@@ -13,8 +13,6 @@ pipeline {
             }
         
             post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
